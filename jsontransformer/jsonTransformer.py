d8406173c339e6cbed87d136f4aea97a5a0eed54
alexchristlieb <alexchristlieb@gmail.com>
2024-04-20 22:21:31 +0200

    Adding date range clustering function

--- jsontransformer/jsonTransformer.py
+++ jsontransformer/jsonTransformer.py
@@ -1,19 +1,33 @@
 import json
 from collections import defaultdict
 
-def categorize_entries(data):
+def categorize_entries(data, date_range_days):
     categories = defaultdict(list)
     for lang, item in data.items():
         for i in item['data']:
-            key = (i['published date'], i['vector']['Country of Incident'])
+            new_key = (i['published date'], i['vector']['Country of Incident'])
             i["language"] = lang
             i["query"] = item['query']
-            categories[key].append(i)
+            
+            found = False
 
-    for (date, country), items in categories.items():
-        print(len(items))
-        # print(categories[(date, country)])
-    # print(categories)
+            # Iterate over existing keys to see if this should be grouped with them
+            for existing_key in list(categories.keys()):
+                existing_date, existing_country = existing_key
+                # Check if the country matches and the date is within the specified range
+                if existing_country == new_key[1] and date_in_range(existing_date, new_key[0], date_range_days):
+                    categories[existing_key].append(i)
+                    found = True
+                    break
+
+            # If no suitable existing category is found, create a new category
+            if not found:
+                categories[new_key].append(i)
+    
+    # Debugging output to check the categorization
+    for key, items in categories.items():
+        print(f"Key: {key}, Entries: {len(items)}")
+
     
     new_data = {}
     for (date, country), items in categories.items():
